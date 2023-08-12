f3a0596935daa25bdf796d218f9fdf28650c4b6b
Shabnamdeep Kaur <122659933+ShabnamdeepKaur@users.noreply.github.com>
2023-08-11 23:02:14 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- METADATA -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 2</title>
+
+    <!-- BOOTSTRAP LINK -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    </body>
+</head>
+
+<body>
+
+    <div class="container mt-5">
+
+        <div class="row bg-warning">
+            <div class="col-md-12 fs-3 text-center">Welcome to the main page</div>
+        </div>
+
+        <div class="row mt-5 d-flex justify-content-evenly">
+            <div class="col-md-3">
+            <div class="card border border-warning border-2" style=" float: left; width: 230px; height: 280px;">
+                    <img src="img/id.png" class="card-img-top mt-4"  style="width: 150px; height: 150px; margin: auto;" alt="profile">
+                    <div class="card-body ">
+                        
+                        <a href="signup.php" class="btn btn-outline-warning mt-4 d-flex justify-content-center">Signup</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-3">
+            <div class="card border border-warning border-2" style=" float: left; width: 230px; height: 280px;">
+                    <img src="img/user-yellow.png" class="card-img-top mt-4"  style="width: 150px; height: 150px; margin: auto;" alt="profile">
+                    <div class="card-body">
+                        
+                        <a href="profile-citizen.html" class="btn btn-outline-warning mt-4 d-flex justify-content-center">Profile</a>
+                    </div>
+                  </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+</body>
+
+</html>
---
+++ success.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Metadata -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Success</title>
+    <!-- Link to CSS -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <main>
+        <div>Welcome to the restricted area! You have Logged in successfully............</div>
+        <div id="welcome">🎀🎀🎀🎀🎀</div>
+    </main>
+</body>
+</html>
